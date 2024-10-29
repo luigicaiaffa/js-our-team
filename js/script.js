b4961892d12ff618f1e99fe009dc75eea3873e2d
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-29 17:38:20 +0100

    add bonus

--- js/script.js
+++ js/script.js
@@ -3,54 +3,58 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png",
+    img: "male1.png",
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png",
+    img: "female1.png",
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png",
+    img: "male2.png",
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png",
+    img: "female2.png",
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png",
+    img: "male3.png",
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png",
+    img: "female3.png",
   },
 ];
 
-// # Funzioni
+// # FUNZIONI
 
-// variabile testo stampa
-let singleCardHtml = ``;
-
-// funzione che crea il contenuto delle card
+/** Funzione che crea il contenuto della card
+ *
+ * @param {*} name nome
+ * @param {*} role ruolo
+ * @param {*} email email
+ * @param {*} img immagine
+ * @returns
+ */
 const cardsContent = (name, role, email, img) => {
-  return (singleCardHtml = ` 
+  return ` 
   <div class="col">
     <div class="card bg-black text-white p-0">
       <div class="row g-0">
         <div class="col-4">
           <img
-            src="./${img}"
+            src="./img/${img}"
             class="img-fluid rounded-start"
             alt="${name}"
           />
@@ -65,24 +69,68 @@ const cardsContent = (name, role, email, img) => {
         </div>
       </div>
     </div>
-  </div>`);
+  </div>`;
 };
 
-// funzione che stampa il contenuto in html
+/** Funzione che stampa il contenuto in Html
+ *
+ * @param {*} listOfMembers lista di oggetti
+ * @param {*} containerHtml elemento Html in cui stampare
+ */
 const stampCardsInHtml = (listOfMembers, containerHtml) => {
   // variabile contenuto finale
   let cardsHtml = ``;
 
   // PER OGNI membro della lista
   listOfMembers.forEach((currentMember) => {
+    // prendine i parametri
     const { name, role, email, img } = currentMember;
+    // crea le card usando la funzione "cardsContent"
     cardsHtml += cardsContent(name, role, email, img);
   });
 
+  // stampa in pagina
   containerHtml.innerHTML = cardsHtml;
 };
 
-// recupero elementi html
-const cardsContainer = document.getElementById("team-cards-box");
+/** Funzione che recupera valori inseriti nel form
+ *
+ * @param {*} listOfMembers lista di oggetti
+ */
+const newMemberValue = (listOfMembers) => {
+  // recupero valori inseriti dall'utente
+  const name = inputName.value;
+  const role = inputRole.value;
+  const email = inputMail.value;
+  const img = inputImage.value;
 
+  // li inserisco in un oggetto che inserisco nella lista dei membri
+  listOfMembers.push({ name, role, email, img });
+};
+
+/** Funzione che svuota il form
+ *
+ */
+const cancFormValue = () => {
+  inputName.value = "";
+  inputRole.value = "";
+  inputMail.value = "";
+  inputImage.value = "";
+};
+
+// # RECUPERO ELEMENTI HTML
+const cardsContainer = document.getElementById("team-cards-box");
+const inputName = document.getElementById("input-name");
+const inputRole = document.getElementById("input-role");
+const inputMail = document.getElementById("input-mail");
+const inputImage = document.getElementById("input-img");
+const inputButton = document.getElementById("input-button");
+
+// # OUTPUT
 stampCardsInHtml(teamMembers, cardsContainer);
+
+inputButton.addEventListener("click", () => {
+  newMemberValue(teamMembers);
+  stampCardsInHtml(teamMembers, cardsContainer);
+  cancFormValue();
+});
